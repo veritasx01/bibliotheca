65efd999598f4fda12e31eb6be13650e73db6fa4
veritas <liorgby2@gmail.com>
2025-08-28 22:25:30 +0300

    dummy data for books

--- services/bookService.js
+++ services/bookService.js
@@ -1,3 +1,4 @@
+import { utilService } from "./util.js";
 /*
 full data model example
 
@@ -63,4 +64,30 @@ export function dummyBookData() {
       },
     },
   ];
+}
+
+function _createBooks() {
+  constctgs = ['Love', 'Fiction', 'Poetry', 'Computers', 'Religion'];
+  constbooks = [];
+  for (let i = 0; i < 20; i++) {
+    constbook = {
+      id: utilService.makeId(),
+      title: utilService.makeLorem(2),
+      subtitle: utilService.makeLorem(4),
+      authors: [utilService.makeLorem(1)],
+      publishedDate: utilService.getRandomIntInclusive(1950, 2024),
+      description: utilService.makeLorem(20),
+      pageCount: utilService.getRandomIntInclusive(20, 600),
+      categories: [ctgs[utilService.getRandomIntInclusive(0, ctgs.length - 1)]],
+      thumbnail: `http://coding-academy.org/books-photos/${i + 1}.jpg`,
+      language: 'en',
+      listPrice: {
+        amount: utilService.getRandomIntInclusive(80, 500),
+        currencyCode: 'EUR',
+        isOnSale: Math.random() > 0.7,
+      },
+    };
+    books.push(book);
+  }
+  console.log('books', books);
 }
